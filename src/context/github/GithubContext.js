77267b49a53cee64e45112c4f7875893b16733b1
RediG7 <guleksiredi7@gmail.com>
2022-03-20 21:35:20 +0100

    Github Context & Reducer

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -12,31 +12,74 @@ export const GithubProvider = ({ children }) => {
 
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  const fetchUsers = async () => {
-    const response = await fetch(`${GITHUB_URL}/users`, {
+  // Get initial users (testing purposes)
+  // const fetchUsers = async () => {
+  //   setLoading();
+
+  //   const response = await fetch(`${GITHUB_URL}/users`, {
+  //     headers: {
+  //       Authorization: `token ${GITHUB_TOKEN}`,
+  //     },
+  //   });
+
+  //   const data = await response.json();
+
+  //   // setUsers(data);
+  //   // setLoading(false);
+  //   dispatch({
+  //     type: "GET_USERS",
+  //     payload: data,
+  //   });
+  // };
+
+  // Set loading
+
+  const searchUsers = async (text) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
 
-    const data = await response.json();
+    const { items } = await response.json();
 
-    // setUsers(data);
-    // setLoading(false);
     dispatch({
       type: "GET_USERS",
-      payload: data,
+      payload: items,
     });
   };
 
+  // Clear users from state
+  const clearUsers = () => {
+    dispatch({
+      type: "CLEAR_USERS",
+    });
+  };
+
+  const setLoading = () =>
+    dispatch({
+      type: "SET_LOADING",
+    });
+
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
